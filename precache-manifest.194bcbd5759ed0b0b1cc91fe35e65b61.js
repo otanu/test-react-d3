2533756edc0ca55f832ffbf90c0e54810e1a6aac
otanu <otanu.t.dev@gmail.com>
2019-03-01 16:34:33 +0900

    Updates

---
+++ precache-manifest.194bcbd5759ed0b0b1cc91fe35e65b61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "437cfd6df15852a143d7",
+    "url": "/static/js/main.437cfd6d.chunk.js"
+  },
+  {
+    "revision": "f1b72a2553afd2d17e43",
+    "url": "/static/js/2.f1b72a25.chunk.js"
+  },
+  {
+    "revision": "437cfd6df15852a143d7",
+    "url": "/static/css/main.178def42.chunk.css"
+  },
+  {
+    "revision": "484324f684fa1c60433fc6393c486978",
+    "url": "/index.html"
+  }
+];
